eb6557f1de4941ae8c4eeb1ebf3a6b5c170587e2
IlliaPysmachenko <106547974+IlliaPysmachenko@users.noreply.github.com>
2023-07-28 15:16:16 +0300

    Продумываю логику для чекбоксов, нужно создать для них слайс

--- src/components/checkbox/Checkbox.tsx
+++ src/components/checkbox/Checkbox.tsx
@@ -1,17 +1,20 @@
 import {FC} from "react";
 
+import style from './Checkbox.module.scss'
+
 
 type CheckboxPropsType = {
     id: any;
     name: string;
+    isChecked: boolean;
 }
 
-const Checkbox: FC<CheckboxPropsType> = ({id, name}) => {
+const Checkbox: FC<CheckboxPropsType> = ({id, name, isChecked}) => {
     return (
-        <>
-            <input type="checkbox" id={id}/>
-            <label htmlFor={id}>{name}</label>
-        </>
+        <div className={style.checkboxContainer}>
+            <input type="checkbox" checked={isChecked} id={id}/>
+            <label htmlFor={id} className={style.checkboxLabel}>{name}</label>
+        </div>
     )
 }
 
--- src/screens/home/HomePage.tsx
+++ src/screens/home/HomePage.tsx
@@ -1,14 +1,44 @@
 import Checkbox from "@/src/components/checkbox/Checkbox";
+import {useState} from "react";
 
+
+const checkboxArr = [
+    {
+        id: 'id1',
+        name:'Checkbox1',
+        isChecked: true,
+    },
+    {
+        id: 'id2',
+        name:'Checkbox2',
+        isChecked: false,
+    },
+    {
+        id: 'id3',
+        name:'Checkbox3',
+        isChecked: true,
+    },
+]
 
 const HomePage = () => {
+
+    const setCheckboxIsCheckedHandler = (id:any, isChecked:boolean) =>{
+
+    }
+
     return (
         <>
             <h1>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur cupiditate debitis dicta, est
                 harum praesentium quod sapiente suscipit tempore ut.
             </h1>
-            <Checkbox id='id1' name='Checkbox1'/>
+            {checkboxArr.map( (checkbox) => {
+                return (
+                    <Checkbox key={checkbox.id} id={checkbox.id} name={checkbox.name} isChecked={checkbox.isChecked}/>
+                )
+            } )}
+
+
         </>
     )
 }
